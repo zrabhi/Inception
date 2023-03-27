734fab7e1fd5cb4f3e3c790f1bb3f7a786fd5c3d
Zakaria Rabhi <zrabhi@e1r4p15.1337.ma>
2023-03-27 21:56:17 +0000

    adding some useful comments

--- requirements/mariadb/tools/setup.sql
+++ requirements/mariadb/tools/setup.sql
@@ -1,9 +1,13 @@
 
-DROP DATABASE IF EXISTS wordpress;
+--  checking if the database exists , if so we drop it
+DROP DATABASE IF EXISTS wordpress; 
+-- creating database
 CREATE DATABASE wordpress;
+--  checking if the user exists , if so we drop it
 DROP USER IF EXISTS zac; 
+-- creating user
 CREATE USER 'zac@localhost' IDENTIFIED BY 'password1';
+-- giveing the user privileges to our database
 GRANT ALL PRIVILEGES ON wordpress.* TO 'zac'@localhost IDENTIFIED BY 'password1';
--- GRANT ALL PRIVILEGES ON 'wordpress' TO 'zac@localhost' IDENTIFIED BY 'password1';
+-- refresh the privileges once new ones have been awarded
 FLUSH PRIVILEGES 
--- CREATE fSER "zrabhi" IF NOT EXIST

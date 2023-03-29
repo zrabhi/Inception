065f5d63917c98904bec59464d6ac7bb92fc782c
Zakaria Rabhi <zrabhi@e1r3p14.1337.ma>
2023-03-29 03:53:35 +0000

    adding env file and docker compose

--- requirements/mariadb/tools/script.sh
+++ requirements/mariadb/tools/script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 service mysql start
+echo "DROP DATABASE IF EXISTS  $USER_DATABASE;" > setup.sql
+echo "CREATE DATABASE $USER_DATABASE ;" > setup.sql
+echo "DROP USER IF EXISTS $USER_WP;" >setup.sql
+echo "CREATE USER $USER_WP@'%' IDENTIFIED BY $USER_PASSWORD;" > setup.sql
+echo "GRANT ALL PRIVILEGES ON $USER_DATABASE.* TO $USER_WP@'%' IDENTIFIED BY $USER_PASSWORD;" > setup.sql 
+echo "FLUSH PRIVILEGES;" > setup.sql
 mysql -u root < /tmp/setup.sql
 exec "$@"
 
--- requirements/mariadb/tools/setup.sql
+++ requirements/mariadb/tools/setup.sql
@@ -1,13 +1,2 @@
 
 --  checking if the database exists , if so we drop it
-DROP DATABASE IF EXISTS wordpress; 
--- creating database
-CREATE DATABASE wordpress;
---  checking if the user exists , if so we drop it
-DROP USER IF EXISTS zac; 
--- creating user
-CREATE USER 'zac'@'%' IDENTIFIED BY 'password1';
--- giveing the user privileges to our database
-GRANT ALL PRIVILEGES ON wordpress.* TO 'zac'@'%' IDENTIFIED BY 'password1';
--- refresh the privileges once new ones have been awarded
-FLUSH PRIVILEGES 

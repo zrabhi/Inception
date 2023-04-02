c16d68f7150e52790e80e2f99b127b2d65eb7b4a
Zakaria Rabhi <zrabhi@e1r8p15.1337.ma>
2023-04-02 03:53:01 +0000

    in wordpress container

---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# wp-cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+
+mv wp-cli.phar /usr/local/bin/wp
+
+cd  /var/www/html
+
+# Checks for WordPress updates via Version Check API.
+
+# wp core <command> means : Downloads, installs, updates, and manages a WordPress installation.
+# wp core check-update
+
+wp core download --allow-root
+
+exec "$@"

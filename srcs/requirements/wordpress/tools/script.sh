c9bff0d532a8b8fdca64d677171c5ed66f8297e2
Zakaria Rabhi <zrabhi@e1r4p15.1337.ma>
2023-04-03 04:01:08 +0000

    minor changes

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # wp-cli
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 
@@ -21,4 +22,6 @@ chown -R www-data:www-data /var/www/html/
 # rm wp-config-sample.php
 # touch ls
 wp core --allow-root config --dbname='wordpress' --dbuser='zac' --dbpass='password1' --dbhost='mariadb' --skip-check
+# sed -i 's/.*listen = \/run\/php\/php7.3-fpm.sock*./listen = 9000\n/' /etc/php/7.3/fpm/pool.d/www.conf
+
 exec "$@"
